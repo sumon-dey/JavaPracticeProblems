ffc7fdcce3e3ae26000f5fd6addbe3131c7bb011
Sumon <sdtechie22@gmail.com>
2019-11-11 09:30:52 +0530

    Added code

---
+++ src/main/java/com/sam/javaPracticeProblemsCode1/ArmstrongNumberCheck.java
@@ -0,0 +1,33 @@
+package com.sam.javaPracticeProblemsCode1;
+
+import java.util.Scanner;
+
+/*
+ * Armstrong Number Examples
+ * 153 -> 1  5  3 -> (1*1*1)+(5*5*5)+(3*3*3) -> 1+125+27 -> 153 (Armstrong Number)
+ * 370 -> 3  7  0 -> (3*3*3)+(7*7*7)+(0*0*0) -> 27+343+0 -> 370 (Armstrong Number)
+ * 142 -> 1  4  2 -> (1*1*1)+(4*4*4)+(2*2*2) -> 1+64+8 -> 73 (Not an Armstrong Number)
+ * */
+
+public class ArmstrongNumberCheck {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number to check whether it is an Armstrong number or not:");
+		int enteredNumber = scanner.nextInt();
+		System.out.println(armstrongNumberCheck(enteredNumber));
+	}
+
+	private static String armstrongNumberCheck(int number) {
+		int sum = 0, value = 0;
+		int temp = number;
+		while (number > 0) {
+			value = number % 10;
+			sum = sum + (value * value * value);
+			number = number / 10;
+		}
+		return sum == temp ? "The number is an Armstrong Number" : "The number is not an Armstrong number";
+	}
+
+}
