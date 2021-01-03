43f3e1d3ea69b21fe21cb6c2d405fe6281131356
Sumon <sdtechie22@gmail.com>
2021-01-03 20:04:03 +0530

    Added String related problems

--- src/main/java/com/sam/javaPracticeProblemsString/ReverseWordsInASentence.java
+++ src/main/java/com/sam/javaPracticeProblemsString/ReverseWordsInASentence.java
@@ -1,10 +1,20 @@
 // reverse the words in a sentence
 package com.sam.javaPracticeProblemsString;
 
+import java.util.Scanner;
+
 public class ReverseWordsInASentence {
 
 	public static void main(String[] args) {
-
+		System.out.println("Enter the sentence: ");
+		Scanner scanner = new Scanner(System.in);
+		String sentence = scanner.nextLine();
+		String[] wordArray = sentence.split(" ");
+		String reversedSentence = "";
+		for (int i = wordArray.length - 1; i >= 0; i--) {
+			reversedSentence += wordArray[i] + " ";
+		}
+		System.out.println("Result: " + reversedSentence);
 	}
 
 }
--- src/main/java/com/sam/javaPracticeProblemsString/StringReversalWithSpacesPositionPreserved.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringReversalWithSpacesPositionPreserved.java
@@ -1,9 +1,19 @@
-// How to reverse a given String with preserving the position of spaces
+// How to reverse a given String with preserving the position of spaces 
+// Example: Input  : "abc de", Output : "edc ba"
 package com.sam.javaPracticeProblemsString;
+
+import java.util.Scanner;
 
 public class StringReversalWithSpacesPositionPreserved {
 
 	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scanner = new Scanner(System.in);
+		String originalString = scanner.nextLine();
+		reverseStringWithSpacePositionPreserved(originalString);
+	}
+
+	public static void reverseStringWithSpacePositionPreserved(String str) {
 
 	}
 
