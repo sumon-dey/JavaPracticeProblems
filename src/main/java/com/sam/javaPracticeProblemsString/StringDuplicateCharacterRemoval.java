ea866f5e20beab06ff9dadb106121a74e8dcfefc
Sumon <sdtechie22@gmail.com>
2021-01-04 17:26:17 +0530

    Added String related problems

--- src/main/java/com/sam/javaPracticeProblemsString/StringDuplicateCharacterRemoval.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringDuplicateCharacterRemoval.java
@@ -1,10 +1,29 @@
 // print the output after removing the duplicate characters from a String
 package com.sam.javaPracticeProblemsString;
 
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class StringDuplicateCharacterRemoval {
 
 	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scanner = new Scanner(System.in);
+		String originalString = scanner.nextLine();
+		stringDuplicateCharacterRemoval(originalString);
+	}
 
+	public static void stringDuplicateCharacterRemoval(String str) {
+		Set<Character> characterSet = new LinkedHashSet<>();
+		for (char c : str.toCharArray()) {
+			characterSet.add(c);
+		}
+		String result = "";
+		for (char c : characterSet) {
+			result += c;
+		}
+		System.out.println("Result: " + result);
 	}
 
 }
