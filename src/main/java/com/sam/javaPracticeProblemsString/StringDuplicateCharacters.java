d9790bb515b2fb95ae3808b5fbf541d26c96d3ba
Sumon <sdtechie22@gmail.com>
2021-01-09 13:53:17 +0530

    Modified code for switch statement

--- src/main/java/com/sam/javaPracticeProblemsString/StringDuplicateCharacters.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringDuplicateCharacters.java
@@ -9,12 +9,12 @@ public class StringDuplicateCharacters {
 	public static void main(String[] args) {
 		String str = "Hello World";
 		printDuplicateCharactersWay1(str);
+		System.out.println("**********************************************");
 		printDuplicateCharactersWay2(str);
 	}
 
 	public static void printDuplicateCharactersWay1(String str) {
 		Map<Character, Integer> counterMap = new HashMap<>();
-		String result = "";
 		for (int i = 0; i < str.length(); i++) {
 			if (counterMap.containsKey(str.charAt(i))) {
 				counterMap.put(str.charAt(i), counterMap.get(str.charAt(i)) + 1);
@@ -25,16 +25,25 @@ public class StringDuplicateCharacters {
 		System.out.println("The duplicate characters are:");
 		for (Map.Entry<Character, Integer> entry : counterMap.entrySet()) {
 			if (entry.getValue() > 1) {
-				System.out.print(" " + entry.getKey());
+				System.out.print(entry.getKey());
 			}
 		}
+		System.out.println();
 	}
 
 	public static void printDuplicateCharactersWay2(String str) {
 		char[] charArray = str.toCharArray();
+		int count;
 		for (int i = 0; i < str.length(); i++) {
+			count = 1;
 			for (int j = i + 1; j < str.length(); j++) {
-
+				if (charArray[i] == charArray[j] && charArray[i] != ' ') {
+					count++;
+					charArray[j] = '0';
+				}
+			}
+			if (count > 1 && charArray[i] != '0') {
+				System.out.print(charArray[i]);
 			}
 		}
 	}
