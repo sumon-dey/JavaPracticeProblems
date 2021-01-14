498a3957c3a4ce518d58e59fc73fdda8730bd6af
Sumon <sdtechie22@gmail.com>
2021-01-14 14:31:47 +0530

    Added String related problems

---
+++ src/main/java/com/sam/javaPracticeProblemsString/StringAllSubstrings.java
@@ -0,0 +1,19 @@
+// print all the non-empty substrings in a given String
+package com.sam.javaPracticeProblemsString;
+
+import java.util.Scanner;
+
+public class StringAllSubstrings {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				System.out.println(str.substring(i, j));
+			}
+		}
+	}
+
+}
--- src/main/java/com/sam/javaPracticeProblemsString/StringLongestPalindromicSubstring.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringLongestPalindromicSubstring.java
@@ -1,10 +1,49 @@
 // find the longest palindromic substring in a given string
 package com.sam.javaPracticeProblemsString;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class StringLongestPalindromicSubstring {
 
 	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		System.out.println("The longest palindromic substring is: " + longestPalindromicSubstring(str));
+	}
 
+	private static String longestPalindromicSubstring(String str) {
+		List<String> palindromeSubstringList = new ArrayList<>();
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++) {
+				if (isPalindrome(str.substring(i, j))) {
+					palindromeSubstringList.add(str.substring(i, j));
+				}
+			}
+		}
+		String longestString = "";
+		for (String s : palindromeSubstringList) {
+			if (s.length() > longestString.length()) {
+				longestString = s;
+			}
+		}
+		return longestString;
+	}
+
+	private static boolean isPalindrome(String str) {
+		char[] charArray = str.toCharArray();
+		for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+			char temp = charArray[i];
+			charArray[i] = charArray[j];
+			charArray[j] = temp;
+		}
+		if (str.equals(String.valueOf(charArray))) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
