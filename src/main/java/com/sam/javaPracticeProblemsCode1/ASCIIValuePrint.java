52471722c2fb777c9b7a1f8932085bada8266ff4
Sumon <sdtechie22@gmail.com>
2021-01-24 11:45:42 +0530

    Added String related problems

--- src/main/java/com/sam/javaPracticeProblemsCode1/ASCIIValuePrint.java
+++ src/main/java/com/sam/javaPracticeProblemsCode1/ASCIIValuePrint.java
@@ -4,7 +4,7 @@ public class ASCIIValuePrint {
 
 	public static void main(String[] args) {
 		for (int i = 0; i <= 127; i++) {
-			System.out.printf("%d is the ASCII vakue of: %c \n", i, i);
+			System.out.printf("%d is the ASCII value of: %c \n", i, i);
 		}
 
 	}
--- src/main/java/com/sam/javaPracticeProblemsString/StringSorting.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringSorting.java
@@ -1,10 +1,39 @@
 // sort a given string in Java
 package com.sam.javaPracticeProblemsString;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class StringSorting {
 
 	public static void main(String[] args) {
+		String str = "hello";
+		sortStringWay1(str);
+		sortStringWay2(str);
+	}
 
+	private static void sortStringWay1(String str) {
+		char[] charArray = str.toCharArray();
+		Arrays.sort(charArray);
+		System.out.println(new String(charArray));
+	}
+
+	private static void sortStringWay2(String str) {
+		Character[] characterArray = new Character[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+			characterArray[i] = str.charAt(i);
+		}
+		Arrays.sort(characterArray, new Comparator<Character>() {
+			@Override
+			public int compare(Character c1, Character c2) {
+				return Character.compare(c1, c2);
+			}
+		});
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Character c : characterArray) {
+			stringBuilder.append(c);
+		}
+		System.out.println(stringBuilder.toString());
 	}
 
 }
