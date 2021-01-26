854973ae937ddb67586fde2076897c27669f7066
Sumon <sdtechie22@gmail.com>
2021-01-26 13:08:35 +0530

    Added String related problems

--- src/main/java/com/sam/javaPracticeProblemsString/StringWhiteSpacesRemoval.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringWhiteSpacesRemoval.java
@@ -1,10 +1,19 @@
 // print after removing all the whitespaces from a String
 package com.sam.javaPracticeProblemsString;
 
+import java.util.Scanner;
+
 public class StringWhiteSpacesRemoval {
 
 	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		removeAllWhitespacesAndPrint(str);
+	}
 
+	private static void removeAllWhitespacesAndPrint(String str) {
+		System.out.println(str.replaceAll("\\s+", ""));
 	}
 
 }
