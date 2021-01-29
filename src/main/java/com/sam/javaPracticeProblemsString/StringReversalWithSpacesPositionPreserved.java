f7de7658c738c32e67b56b37453a403adb615ca2
Sumon <sdtechie22@gmail.com>
2021-01-29 22:32:11 +0530

    Added String related problems

--- src/main/java/com/sam/javaPracticeProblemsString/StringReversalWithSpacesPositionPreserved.java
+++ src/main/java/com/sam/javaPracticeProblemsString/StringReversalWithSpacesPositionPreserved.java
@@ -14,7 +14,24 @@ public class StringReversalWithSpacesPositionPreserved {
 	}
 
 	public static void reverseStringWithSpacePositionPreserved(String str) {
-
+		char[] originalCharArray = str.toCharArray();
+		char[] newCharArray = new char[str.length()];
+		for (int i = 0; i < originalCharArray.length; i++) {
+			if (originalCharArray[i] == ' ') {
+				newCharArray[i] = ' ';
+			}
+		}
+		int j = str.length() - 1;
+		for (int i = 0; i < originalCharArray.length; i++) {
+			if (originalCharArray[i] != ' ') {
+				if (newCharArray[j] == ' ') {
+					j--;
+				}
+				newCharArray[j] = originalCharArray[i];
+				j--;
+			}
+		}
+		System.out.println("Result: " + new String(newCharArray));
 	}
 
 }
